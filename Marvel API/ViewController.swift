345067cf66622c4422108c42356d47859edc0c8b
Alba Rico <amiloyrico.36@gmail.com>
2022-11-08 15:38:58 -0500

    Initial Commit

---
+++ Marvel API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel API
+//
+//  Created by Alba Yolima Rico Montenegro on 8/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
